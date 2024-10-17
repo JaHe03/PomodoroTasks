0b08a535235f6b3fe1c98a0865c73185c2787b73
Jaryn Hernandez <tun62036@temple.edu>
2024-10-17 11:34:06 -0400

    nextSession

--- frontEnd/scripts/index.js
+++ frontEnd/scripts/index.js
@@ -1,7 +1,8 @@
 // index.js
-
 import { startTimer, pauseTimer, resetTimer, setTimer, getCustomDurations } from './timer.js';
 import { setTheme } from './themes.js';
+import { addTask } from './tasks.js';
+
 // Get elements from the DOM
 const pomodoroBtn = document.getElementById('pomodoroBtn');
 const shortBreakBtn = document.getElementById('shortBreakBtn');
@@ -11,6 +12,13 @@ const resetBtn = document.getElementById('resetBtn');
 const lightModeBtn = document.getElementById('lightModeBtn');
 const darkModeBtn = document.getElementById('darkModeBtn');
 const defaultModeBtn = document.getElementById('defaultModeBtn');
+const taskList = document.getElementById('taskList');
+const taskInput = document.getElementById('taskInput');
+const taskForm = document.getElementById('taskForm');
+const deleteTaskBtn = document.getElementById('deleteTaskBtn');
+const moveUpTaskBtn = document.getElementById('moveUpTaskBtn');
+const moveDownTaskBtn = document.getElementById('moveDownTaskBtn');
+
 
 // Event listeners for buttons
 pomodoroBtn.addEventListener('click', () => {
@@ -41,4 +49,7 @@ lightModeBtn.addEventListener('click', () => setTheme('light'));
 darkModeBtn.addEventListener('click', () => setTheme('dark'));
 defaultModeBtn.addEventListener('click', () => setTheme('default'));
 
-
+taskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addTask(taskList, taskInput);
+});
